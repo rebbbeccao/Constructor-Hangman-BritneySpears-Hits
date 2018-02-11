5f8215bbdc777fdee3b624ed30a01ab55a056e5d
Rebecca A. Olson <rebeccaao@Rebeccas-MacBook-Air.local>
2018-02-11 15:32:56 -0800

    completed letter.js

---
+++ library/letter.js
@@ -0,0 +1,27 @@
+function Letter(char) {
+    // If a character is not a number or a letter, make it visible right away
+    this.visible = !/[a-z1-9]/i.test(char);
+    // Save the underlying character
+    this.char = char;
+}
+
+Letter.prototype.toString = function() {
+    if (this.visible === true) {
+        return this.char;
+    }
+    return '_';
+};
+
+Letter.prototype.getSolution = function() {
+    return this.char;
+};
+
+Letter.prototype.guess = function(charGuess) {
+    if (charGuess.toUpperCase() === this.char.toUpperCase()) {
+        this.visible = true;
+        return true;
+    }
+    return false;
+};
+
+module.exports = Letter;
