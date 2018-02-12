eae0ebe083b736708436b7eec930a377a6bf76ae
Rebecca A. Olson <rebeccaao@Rebeccas-MacBook-Air.local>
2018-02-11 16:21:16 -0800

    completed letter.js finishing game.js

--- library/game.js
+++ library/game.js
@@ -1,11 +1,14 @@
 var inquirer = require("inquirer");
 var chalk = require("chalk");
+var Word = require("./word");
+var words = require("./words");
 
-var Game = function() {
+function Game() {
+
     var self = this;
 
     this.play = function() {
-        this.guessLeft = 10;
+        this.guessesLeft = 10;
         this.nextWord();
     };
 
@@ -14,7 +17,57 @@ var Game = function() {
         this.currentWord = new Word(randword);
         console.log('\n' + this.currentWord + '\n');
         this.makeGuess();
+    };
+
+    this.makeGuess = function() {
+        this.askLetter().then(function() {
+            if (self.guessesLeft < 1) {
+                console.log('No guesses remaining! Word to guess was: ' + self.currentWord.getSolution() + '\n');
+                self.playAgain();
+            }
+            else if (self.currentWord.guessedCorrectly()) {
+                console.log("You got it right! Next word...");
+                self.guessesLeft = 10;
+                self.nextWord();
+            } 
+            else {
+                self.makeGuess();
+            }
+        });
     }
 
+    this.playAgain = function() {
+        inquirer.prompt([
+            {
+                type: 'confirm',
+                name: 'choice',
+                message: 'Play Again?'
+            }
+        ])
+        .then(function(val) {
+            if (val.choice) {
+                self.play();
+            }
+            else {
+                self.quit();
+            }
+        });
+    };
+
+    this.askLetter = function() {
+        return inquirer.prompt([
+            {
+                type: "input",
+                name: "choice",
+                message: "Guess a letter!",
+                validate: function(val) {
+                    return /[a-z1-9]/gi.test(val);
+                }
+            }
+        ])
+        .then(function(val) {
+            
+        })
+    }
 }
 
