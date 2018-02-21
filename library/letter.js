198f19c78614dcb4a3828859674207c35b1fb42a
Rebecca A. Olson <rebeccaao@Rebeccas-MacBook-Air.local>
2018-02-21 15:16:27 -0800

    updates pseudo code

--- library/letter.js
+++ library/letter.js
@@ -7,10 +7,11 @@ function Letter(char) {
     // Save the underlying character
     this.char = char;
     console.log("char = " + char);
-};
+}
 
 // Returns either an underscore or the underlying character depending on 'this.visible'
-    // Because we call this function toString 
+    // Because we call this function toString, when we call "this.letters.join"
+    // in Word.js, Javascript will automatically use the value we return here. 
 Letter.prototype.toString = function() { 
     if (this.visible === true) {
         // console.log('this.char = ' + this.char);
@@ -24,6 +25,7 @@ Letter.prototype.getSolution = function() {
     return this.char;
 };
 
+// Accepts a user's guess
 Letter.prototype.guess = function(charGuess) {
     if (charGuess.toUpperCase() === this.char.toUpperCase()) {
         this.visible = true;
