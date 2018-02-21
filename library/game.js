e25ec97f70cd309a593f719c02d80d866e01b9d8
Rebecca A. Olson <rebeccaao@MacBook-Air.localdomain>
2018-02-21 13:48:08 -0800

    pseudocode & uodates

--- library/game.js
+++ library/game.js
@@ -1,110 +1,127 @@
 var inquirer = require("inquirer");
 var chalk = require("chalk");
 var Word = require("./word");
-// var Letter = requrie("./letter");
-// var words = require("./words");
+
+// Game constructor is responsible for keeping score
+// & controlling the flow of the overall game
 
 function Game() {
+  // Saved as reference because 'this' will change in Inquirer
+  var self = this;
+  // Sets # guesses remaining and gets next word
+  this.play = function() {
+    this.guessesLeft = 10;
+    this.nextWord();
+  };
 
-    var self = this;
+  // Creates a new Word object using a random word from the array
+  this.nextWord = function() {
+    var wordBank = [
+      "Lucky",
+      "Baby One More Time",
+      "Womanizer",
+      "Til The World Ends",
+      "Circus",
+      "I Wanna Go",
+      "Hold It Against Me",
+      "Toxic",
+      "Oops I Did It Again",
+      "Gimme More",
+      "You Drive Me Crazy",
+      "Everytime",
+      "If U Seek Amy",
+      "Piece Of Me",
+      "Sometimes",
+      "Stronger"
+    ];
+    var randWord_int = Math.floor(Math.random() * wordBank.length);
+    var randWord = wordBank[randWord_int];
+    this.currentWord = new Word(randWord);
+    console.log("\n" + this.currentWord + "\n");
+    // Asks user for their guess
+    this.makeGuess();
+  };
 
-    this.play = function() {
-        this.guessesLeft = 10;
-        this.nextWord();
-    };
-//randWord is erroring as 'undefined'
-    this.nextWord = function() {
-        var wordBank = [
-            "Lucky",
-            "Baby One More Time",
-            "Womanizer",
-            "Til The World Ends",
-            "Circus",
-            "I Wanna Go",
-            "Hold It Against Me",
-            "Toxic",
-            "Oops I Did It Again",
-            "Gimme More",
-            "You Drive Me Crazy",
-            "Everytime",
-            "If U Seek Amy",
-            "Piece Of Me",
-            "Sometimes",
-            "Stronger"
-        ];
-        var randWord_int = Math.floor(Math.random() * wordBank.length);
-        var randWord = wordBank[randWord_int];
-        this.currentWord = new Word(randWord);
-        console.log('\n' + this.currentWord + '\n');
-        this.makeGuess();
-    };
 
-        this.makeGuess = function() {
-        this.askLetter().then(function() {
-            if (self.guessesLeft < 1) {
-                console.log('No guesses remaining! Word to guess was: ' + self.currentWord.getSolution() + '\n');
-                self.playAgain();
-            }
-            else if (self.currentWord.guessedCorrectly()) {
-                console.log("You got it right! Next word...");
-                self.guessesLeft = 10;
-                self.nextWord();
-            } 
-            else {
-                self.makeGuess();
-            }
-        });
-    }
+  // Uses Inquirer to prompt the user for their guess
+  this.makeGuess = function() {
+    this.askLetter().then(function() {
+      // If the user has no guesses left after this guess, 
+      // show them the word & ask if they want to play again  
+      if (self.guessesLeft < 1) {
+        console.log(
+          "No guesses remaining! Word to guess was: " +
+            self.currentWord.getSolution() +
+            "\n"
+        );
+        self.playAgain();
+        // If user guessed all of the letters correctly, reset guessesLeft
+        // & get the next word
+      } else if (self.currentWord.guessedCorrectly()) {
+        console.log("You got it right! Next word...");
+        self.guessesLeft = 10;
+        self.nextWord();
+        // Otherwise prompt the user to guess the next letter
+      } else {
+        self.makeGuess();
+      }
+    });
+  };
 
-    this.playAgain = function() {
-        inquirer.prompt([
-            {
-                type: 'confirm',
-                name: 'choice',
-                message: 'Play Again?'
-            }
-        ])
-        .then(function(val) {
-            if (val.choice) {
-                self.play();
-            }
-            else {
-                self.quit();
-            }
-        });
-    };
+  // Asks the user if they want to playa again after running out of guessesLeft
+  this.playAgain = function() {
+    inquirer
+      .prompt([
+        {
+          type: "confirm",
+          name: "choice",
+          message: "Play Again?"
+        }
+      ])
+      .then(function(val) {  
+        if (val.choice) {
+          self.play();
+        } else {
+          self.quit();
+        }
+      });
+  };
 
-    this.askLetter = function() {
-        return inquirer.prompt([
-            {
-                type: "input",
-                name: "choice",
-                message: "Guess a letter!",
-                // validate: function(val) {
-                //     return /[a-z1-9]/gi.test(val);
-                //     console.log("val= " + val);
-                // }
-            }
-        ])
-        .then(function(val) {
-            var didGuessCorrectly = self.currentWord.guessLetter(val.choice);
-            console.log("didGuessCorrectly= " + didGuessCorrectly);
-            if (didGuessCorrectly) {
-                console.log(chalk.green("\nCORRECT!!!\n"));
-            }
-            else {
-                self.guessesLeft--;
-                console.log(chalk.red("\nINCORRECT!!!\n"));
-                console.log(self.guessesLeft + " guesses remaining!!!");
-            }
-        });
-    };
+  // Prompts the user for a letter
+  this.askLetter = function() {
+    return inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "choice",
+          message: "Guess a letter!",
+          validate: function(val) {
+              return /[a-z1-9]/gi.test(val);
+            //   console.log("val= " + val);
+          }
+        }
+      ])
+      .then(function(val) {
+        // If the user's guess is in the current word, log that they chose correctly
+        var didGuessCorrectly = self.currentWord.guessLetter(val.choice);
+        console.log("didGuessCorrectly= " + didGuessCorrectly);
+        if (didGuessCorrectly) {
+          console.log(chalk.green("\nCORRECT!!!\n"));
 
-    this.quit = function() {
-        console.log("\nGoodbye!");
-        process.exit(0);
-    };
-};
+        // Oherwise decrement "guessesLeft", and let the user know how many
+        // guesses they have left
+        } else {
+          self.guessesLeft--;
+          console.log(chalk.red("\nINCORRECT!!!\n"));
+          console.log(self.guessesLeft + " guesses remaining!!!");
+        }
+      });
+  };
+
+  this.quit = function() {
+    console.log("\nGoodbye!");
+    process.exit(0);
+  };
+}
 
 module.exports = Game;
-
--- library/letter.js
+++ library/letter.js
@@ -1,14 +1,16 @@
+// The letter constructor is responsible for displaying either 
+// an underscore or the underlying character for each letter in the word
+
 function Letter(char) {
     // If a character is not a number or a letter, make it visible right away
-    
     this.visible = !/[a-z1-9]/i.test(char);
     // Save the underlying character
-    
     this.char = char;
-    console.log("char= " + char);
+    console.log("char = " + char);
 };
 
-//If chosen word contains any symbols/numbers display them 
+// Returns either an underscore or the underlying character depending on 'this.visible'
+    // Because we call this function toString 
 Letter.prototype.toString = function() { 
     if (this.visible === true) {
         // console.log('this.char = ' + this.char);
